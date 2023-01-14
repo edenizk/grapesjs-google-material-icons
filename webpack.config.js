e8f98a41d338b31cb08dffd6951a307cefff73e1
Ergin Deniz Kosecioglu <ergindenizkosecioglu@gmail.com>
2023-01-14 14:19:59 +0100

    Fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,9 @@ export default ({ config }) => ({
     library: 'gjs-google-material-icons',
   },
   module: {
+    ...config.module,
     rules: [
+      ...config.module.rules,
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
