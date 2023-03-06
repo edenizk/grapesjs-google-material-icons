7c3443726f9490d8018fe849d555b0e505e33425
Ergin Deniz Kosecioglu <ergindenizkosecioglu@gmail.com>
2023-03-06 22:53:19 +0100

    Add filter data from json

--- src/IconPickerPopupType.ts
+++ src/IconPickerPopupType.ts
@@ -1,5 +1,10 @@
 import IconPickerPopup from './IconPickerPopup'
 import IEditor from './types/editor';
+import materialIcons from '../_data/versions.json'
+
+interface IIcons {
+  [name: string]: number
+}
 
 export const iconPickerPopupType = (editor: IEditor) => {
   editor.Commands.add('open:icon-picker', {
@@ -11,6 +16,8 @@ export const iconPickerPopupType = (editor: IEditor) => {
       if (editorEl) {
         editorEl.insertAdjacentHTML('beforebegin', IconPickerPopup(editor));
         
+        this.renderList(materialIcons);
+
         console.log('editorEl', editorEl)
         const closeEl = document.querySelectorAll('.googleIconPicker__close');
         console.log('closeEl', closeEl)
@@ -31,6 +38,41 @@ export const iconPickerPopupType = (editor: IEditor) => {
         }
       }
     },
+    renderList(icons: IIcons) {
+      console.log('icons', icons);
+      const iconList = document.querySelector('.googleIconPicker__icons');
+
+      if (iconList) {
+        iconList.innerHTML = '';
+        Object.keys(icons).forEach((icon) => {
+          iconList.appendChild(this.renderIcon(icon));
+        });
+      }
+    },
+    create(tag: string) {return document.createElement(tag)},
+
+    renderIcon(name: string) {
+      const button = document.createElement('button');
+      button.setAttribute('aria-haspopup', 'dialog');
+      button.setAttribute('role', 'option');
+      button.setAttribute('aria-label', 'Search Icon');
+      button.setAttribute('aria-selected', 'false');
+      button.classList.add('googleIconPicker__iconWrapper');
+
+      const span1 = document.createElement('span');
+      span1.classList.add('googleIconPicker__icon', 'material-symbols-outlined');
+      span1.style.fontSize = '48px';
+      span1.innerText = name;
+
+      const span2 = document.createElement('span');
+      span2.classList.add('googleIconPicker__iconName');
+      span2.innerText = name.replaceAll('_', ' ');
+
+      button.appendChild(span1);
+      button.appendChild(span2);
+
+      return button;
+    },
     debounce(func: Function, delay: number) {
       let timerId: ReturnType<typeof setTimeout>;
       return function(this: any, ...args: any[]) {
@@ -49,12 +91,19 @@ export const iconPickerPopupType = (editor: IEditor) => {
       const debouncedSearch = this.debounce(() => {
         const filter = input.value.trim().toLowerCase();
 
-        iconEls.forEach(iconEl => {
-          const item = iconEl.querySelector("span");
-          const txtValue = item?.textContent?.trim().toLowerCase();
-          console.log('txtValue', txtValue);
-          iconEl.style.display = txtValue?.includes(filter) ? "" : "none";
-        });
+        console.log('filter', filter)
+        const filteredIcons = Object.keys(materialIcons)
+        .filter((key) => key.includes(filter))
+        .reduce((cur, key) => { return Object.assign(cur, { [key]: materialIcons[key as keyof typeof materialIcons] })}, {});
+        
+        this.renderList(filteredIcons);
+
+        // iconEls.forEach(iconEl => {
+        //   const item = iconEl.querySelector("span");
+        //   const txtValue = item?.textContent?.trim().toLowerCase();
+        //   console.log('txtValue', txtValue);
+        //   iconEl.style.display = txtValue?.includes(filter) ? "" : "none";
+        // });
       }, 100);
       
       debouncedSearch();
